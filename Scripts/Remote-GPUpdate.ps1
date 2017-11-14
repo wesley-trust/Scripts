ff6ecb69a3727ceb7cba57b4761da08d9584fb3a
Wesley Trust <wesley.trust@rosslynanalytics.com>
2017-11-14 21:37:27 +0000

    Update script to execute as a function, using a safe verb

--- Scripts/Remote-GPUpdate.ps1
+++ Scripts/Remote-GPUpdate.ps1
@@ -3,73 +3,83 @@
 #Script name: Script to execute gpupdate remotely on computers
 #Creator: Wesley Trust
 #Date: 2017-11-14
-#Revision: 1
+#Revision: 2
 #References: 
 
 .Synopsis
-    A script that resolves the domain controller, gets computers within an OU and executes gpupdate on each remotely.
+    A script that resolves the domain controller, gets computers within an OU then executes gpupdate on each remotely.
 .DESCRIPTION
-
+    A script that executes a function that resolves the domain controller,
+    (defaults to current DNS domain, assumes domain controller is DNS server),
+    gets computers within an OU on that domain controller, then executes gpupdate on each computer remotely.
 #>
 
-# Parameters
-Param(
-    [Parameter(
-        Mandatory=$True,
-        HelpMessage="Enter the fully qualified domain name"
-    )]
-    [String]
-    $Domain,
-    
-    [Parameter(
-        Mandatory=$True,
-        HelpMessage="Enter the OU to select computers"
-    )]
-    [String]
-    $OU
-)
+Function Invoke-Remote-GPUpdate () {
 
-# Authentication Variable
-$Credential = Get-Credential
-
-try {
-    
-    # Update variable with the start of authority object
-    $Domain = Resolve-DnsName $Domain -Type SOA
-
-    # Create session to primary server of domain
-    $Session = New-PSSession -ComputerName $Domain.PrimaryServer -Credential $Credential
-
-    # Invoke remote command within open session
-    $Computers = Invoke-Command -Session $Session -ErrorAction Stop -ScriptBlock {
+    # Parameters
+    Param(
+        [Parameter(
+            Mandatory=$True,
+            HelpMessage="Enter the fully qualified domain name"
+        )]
+        [String]
+        $Domain = $ENV:USERDNSDOMAIN,
         
-                #Get Servers within OU
-                Get-ADComputer -Filter * -SearchBase $OU
-    }
+        [Parameter(
+            Mandatory=$True,
+            HelpMessage="Enter the OU to select computers"
+        )]
+        [String]
+        $OU
+    )
 
-    # Remove session
-    Remove-PSSession -Session $Session
+    # Authentication Variable
+    $Credential = Get-Credential
 
-    # For each computer in variable
-    foreach ($Computer in $Computers){
+    try {
+        
+        # Update variable with the start of authority object
+        $Domain = Resolve-DnsName $Domain -Type SOA
 
-        # Create session
-        $Session = New-PSSession -ComputerName $Computer.dnshostname -Credential $Credential
+        # Create session to primary server of domain
+        $Session = New-PSSession -ComputerName $Domain.PrimaryServer -Credential $Credential
 
-        # Run command in remote session for server
-        Invoke-Command -Session $Session -ScriptBlock {
-
-            gpupdate /force
-
+        # Invoke remote command within open session
+        $Computers = Invoke-Command -Session $Session -ErrorAction Stop -ScriptBlock {
+            
+            #Get Servers within OU
+            Get-ADComputer -Filter * -SearchBase $OU
         }
 
         # Remove session
         Remove-PSSession -Session $Session
 
+        # For each computer in variable
+        foreach ($Computer in $Computers){
+
+            # Create session
+            $Session = New-PSSession -ComputerName $Computer.dnshostname -Credential $Credential
+
+            # Run command in remote session for server
+            Invoke-Command -Session $Session -ScriptBlock {
+
+                gpupdate /force
+
+            }
+
+            # Remove session
+            Remove-PSSession -Session $Session
+
+        }
+    }
+
+    catch {
+        
+        # Catch exceptions
+        Write-Error -Message $_.Exception
+        throw $_.Exception
     }
 }
 
-catch {
-    Write-Error -Message $_.Exception
-    throw $_.Exception
-}
+# Execute Script, defaults to current user DNS domain
+Invoke-Remote-GPUpdate

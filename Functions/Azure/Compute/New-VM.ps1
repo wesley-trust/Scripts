428961ca4c9d16c7524ccf0b86e55365e2b7601f
Wesley Trust <wesley.trust@rosslynanalytics.com>
2017-11-18 19:12:58 +0000

    Fix parameters, remove 'to do' comments, reformat commands for easier viewing.

--- Functions/Azure/Compute/New-VM.ps1
+++ Functions/Azure/Compute/New-VM.ps1
@@ -17,7 +17,6 @@
 #>
 
 function New-VM() {
-    #Parameters
     Param(
         [Parameter(
             Mandatory=$false,
@@ -25,154 +24,132 @@ function New-VM() {
         )]
         [string]
         $SubscriptionID,
-        
         [Parameter(
             Mandatory=$false,
             HelpMessage="Enter the resource group name"
         )]
         [string]
         $ResourceGroupName,
-               
         [Parameter(
             Mandatory=$false,
             HelpMessage="Enter the publisher name"
         )]
         [string]
         $PublisherName = "MicrosoftWindowsServer",
-
         [Parameter(
             Mandatory=$false,
             HelpMessage="Enter the Azure offer"
         )]
         [string]
         $Offer = "WindowsServer",
-
         [Parameter(
             Mandatory=$false,
             HelpMessage="Enter the SKU"
         )]
         [string]
         $SKU = "2012-R2-Datacenter",
-
         [Parameter(
             Mandatory=$false,
             HelpMessage="Enter whether to use latest version"
         )]
         [string]
         $Latest = "latest",
-
         [Parameter(
             Mandatory=$false,
             HelpMessage="Enter the Azure region location"
         )]
         [string]
         $Location,
-
         [Parameter(
             Mandatory=$true,
             HelpMessage="Enter the VM name"
         )]
         [string]
         $VMName,
-
         [Parameter(
             Mandatory=$false,
             HelpMessage="Enter the VM size"
         )]
         [string]
         $VMSize,
-
         [Parameter(
             Mandatory=$true,
             HelpMessage="Enter the Storage Account Type"
         )]
         [string]
         $StorageType,
-
         [Parameter(
             Mandatory=$false,
             HelpMessage="Enter the subnet name"
         )]
         [string]
         $SubnetName = "default",
-
         [Parameter(
             Mandatory=$false,
             HelpMessage="Enter the virtual network name"
         )]
         [string]
         $VNetName,
-
         [Parameter(
             Mandatory=$false,
             HelpMessage="Enter the virtual network address prefix"
         )]
         [string]
         $VNetAddressPrefix,
-
         [Parameter(
             Mandatory=$false,
             HelpMessage="Enter the virtual network subnet address prefix"
         )]
         [string]
         $VNetSubnetAddressPrefix,
-
         [Parameter(
             Mandatory=$false,
             HelpMessage="Enter the number of data disks (if any)"
         )]
         [int]
         $DataDisk,
-
         [Parameter(
             Mandatory=$false,
             HelpMessage="Enter the size of each data disks (if any)"
         )]
         [int]
         $DataDiskSize,
-        
         [Parameter(
             Mandatory=$false,
             HelpMessage="Specify whether to provision a public IP address"
         )]
-        [int]
+        [bool]
         $ProvisionPIP,
-        
         [Parameter(
             Mandatory=$false,
             HelpMessage="Specifies whether the VM should run post provisioning"
         )]
-        [int]
+        [bool]
         $PostProvision,
-
         [Parameter(
             Mandatory=$false,
             HelpMessage="Enter the post provision script name"
         )]
         [string]
         $ScriptName,
-
         [Parameter(
             Mandatory=$false,
             HelpMessage="Enter the post provision script storage account"
         )]
         [string]
         $ScriptStorageAccount,
-
         [Parameter(
             Mandatory=$false,
             HelpMessage="Enter the post provision script storage account key"
         )]
         [string]
         $ScriptStorageAccountKey,
-
         [Parameter(
             Mandatory=$false,
             HelpMessage="Enter the post provision script file name"
         )]
         [string]
         $ScriptFileName,
-
         [Parameter(
             Mandatory=$false,
             HelpMessage="Enter the post provision script storage container name"
@@ -240,7 +217,10 @@ function New-VM() {
             $VMObject = $null
 
             # Check if a VM exists with specified name
-            $VMObject = Get-AzureRMVM -ResourceGroupName $ResourceGroupName -VMName $VMName -ErrorAction SilentlyContinue
+            $VMObject = Get-AzureRMVM `
+                -ResourceGroupName $ResourceGroupName `
+                -VMName $VMName `
+                -ErrorAction SilentlyContinue
             
             # While a VM exists with the same name
             while ($VMObject){
@@ -260,7 +240,10 @@ function New-VM() {
                 }
                 
                 # Recheck if VM exists with new name
-                $VMObject = Get-AzureRMVM -ResourceGroupName $ResourceGroupName -VMName $VMName -ErrorAction SilentlyContinue
+                $VMObject = Get-AzureRMVM `
+                    -ResourceGroupName $ResourceGroupName `
+                    -VMName $VMName `
+                    -ErrorAction SilentlyContinue
             }
 
             # Get supported sizes in location of VM
@@ -287,30 +270,49 @@ function New-VM() {
             if ($ProvisionPIP){
                 
                 # Create public IP
-                $PIp = New-AzureRmPublicIpAddress -Name $VMName -ResourceGroupName $ResourceGroupName -Location $Location -AllocationMethod Dynamic
+                $PIp = New-AzureRmPublicIpAddress `
+                    -Name $VMName `
+                    -ResourceGroupName $ResourceGroupName `
+                    -Location $Location `
+                    -AllocationMethod Dynamic
                 
                 # Create VM Network Interface with PIP
-                $Interface = New-AzureRmNetworkInterface -Name $VMName -ResourceGroupName $ResourceGroupName -Location $Location -SubnetId $VNet.Subnets[0].Id -PublicIpAddressId $PIp.Id
+                $Interface = New-AzureRmNetworkInterface `
+                    -Name $VMName -ResourceGroupName $ResourceGroupName `
+                    -Location $Location `
+                    -SubnetId $VNet.Subnets[0].Id `
+                    -PublicIpAddressId $PIp.Id
                         
             }
             Else {
                 
                 # Create VM Network Interface without a public IP
-                $Interface = New-AzureRmNetworkInterface -Name $VMName -ResourceGroupName $ResourceGroupName -Location $Location -SubnetId $VNet.Subnets[0].Id
+                $Interface = New-AzureRmNetworkInterface `
+                    -Name $VMName `
+                    -ResourceGroupName $ResourceGroupName `
+                    -Location $Location `
+                    -SubnetId $VNet.Subnets[0].Id
             }
-            
-            # Enable diagnostics
-
-            # Consider Availability group
 
             # Create virtual machine configuration object
             $VirtualMachine = New-AzureRmVMConfig -VMName $VMName -VMSize $VMSize
 
             # Set Operating System settings
-            $VirtualMachine = Set-AzureRmVMOperatingSystem -VM $VirtualMachine -Windows -ComputerName $VMName -Credential $VMCredential -ProvisionVMAgent -EnableAutoUpdate
+            $VirtualMachine = Set-AzureRmVMOperatingSystem `
+                -VM $VirtualMachine `
+                -Windows `
+                -ComputerName $VMName `
+                -Credential $VMCredential `
+                -ProvisionVMAgent `
+                -EnableAutoUpdate
             
             # Set image used for operating system
-            $VirtualMachine = Set-AzureRmVMSourceImage -VM $VirtualMachine -PublisherName $PublisherName -Offer $Offer -Skus $SKU -Version $latest
+            $VirtualMachine = Set-AzureRmVMSourceImage `
+                -VM $VirtualMachine `
+                -PublisherName $PublisherName `
+                -Offer $Offer `
+                -Skus $SKU `
+                -Version $latest
             
             # Set network interface
             $VirtualMachine = Add-AzureRmVMNetworkInterface -VM $VirtualMachine -Id $Interface.Id
@@ -329,16 +331,32 @@ function New-VM() {
                 1..$DataDisk | ForEach-Object {
                     
                     # Create Managed Data Disk
-                    $DiskConfig = New-AzureRmDiskConfig -AccountType $StorageType -Location $Location -CreateOption Empty -OsType Windows -DiskSizeGB $DataDiskSize
-                    $Disk = New-AzureRmDisk -Disk $DiskConfig -ResourceGroupName $resourceGroupName -DiskName $VMName'DataDisk'$_ 
+                    $DiskConfig = New-AzureRmDiskConfig `
+                        -AccountType $StorageType `
+                        -Location $Location `
+                        -CreateOption Empty `
+                        -OsType Windows `
+                        -DiskSizeGB $DataDiskSize
+
+                    $Disk = New-AzureRmDisk `
+                        -Disk $DiskConfig `
+                        -ResourceGroupName $resourceGroupName `
+                        -DiskName $VMName'DataDisk'$_ 
                     
                     # Attach to Virtual machine object
-                    $VirtualMachine = Add-AzureRmVMDataDisk -VM $VirtualMachine -CreateOption Attach -ManagedDiskId $Disk.Id -Lun $_
+                    $VirtualMachine = Add-AzureRmVMDataDisk `
+                        -VM $VirtualMachine `
+                        -CreateOption Attach `
+                        -ManagedDiskId $Disk.Id -Lun $_
                 }
             }
 
             # Set OS disk to create from Windows image
-            $VirtualMachine = Set-AzureRmVMOSDisk -VM $VirtualMachine -StorageAccountType $StorageType -CreateOption FromImage -Windows
+            $VirtualMachine = Set-AzureRmVMOSDisk `
+                -VM $VirtualMachine `
+                -StorageAccountType $StorageType `
+                -CreateOption FromImage `
+                -Windows
 
             # Create VM in Azure
             New-AzureRmVM -ResourceGroupName $ResourceGroupName -Location $Location -VM $VirtualMachine

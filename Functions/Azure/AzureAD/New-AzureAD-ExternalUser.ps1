17c41eb84176b4af99616a90114d71d303364f58
Wesley Trust <wesley.trust@rosslynanalytics.com>
2017-12-03 15:55:27 +0000

    Initial function to create external Azure AD user

---
+++ Functions/Azure/AzureAD/New-AzureAD-ExternalUser.ps1
@@ -0,0 +1,124 @@
+<#
+#Script name: New-AzureAD-ExternalUser
+#Creator: Wesley Trust
+#Date: 2017-12-03
+#Revision: 1
+#References: 
+
+.Synopsis
+    Function that connects to an Azure AD tenant, invites external user and sets directory user type (by default to Member).
+.Description
+
+.Example
+    New-AzureAD-ExternalUser -Credential $Credential -Emails "wesley.trust@example.com" -UserType $UserType
+.Example
+    
+#>
+
+function New-AzureAD-ExternalUser() {
+    Param(
+        [Parameter(
+            Mandatory=$false,
+            HelpMessage="Specify a PowerShell credential"
+        )]
+        [pscredential]
+        $Credential,
+        [Parameter(
+            Mandatory=$false,
+            HelpMessage="Specify email address(es)"
+        )]
+        [string[]]
+        $Emails,
+        [Parameter(
+            Mandatory=$false,
+            HelpMessage="Specify user type (Default: Member)"
+        )]
+        [string]
+        $UserType = "Member",
+        [Parameter(
+            Mandatory=$false,
+            HelpMessage="Specify whether to skip authentication"
+        )]
+        [bool]
+        $SkipAuthentication = $false
+    )
+
+    Begin {
+        try {
+            # Variables
+            $Module = "AzureAD"
+            
+            # Check if module is installed
+            $ModuleCheck = Get-Module -ListAvailable | Where-Object Name -eq $Module
+            
+            # If not installed, install the module
+            if (!$ModuleCheck){
+                Install-Module -Name $Module -AllowClobber -Force -ErrorAction Stop
+            }
+            
+            # Connect to directory tenant
+            if (!$SkipAuthentication) {
+                Connect-AzureAD -Credential $Credential
+            } 
+        }
+        catch {
+            Write-Error -Message $_.Exception
+            throw $_.exception
+        }
+    }
+    
+    Process {
+        try {
+            # Get Tenant domain
+            $Tenant = Get-AzureADDomain | Where-Object IsInitial -eq $true
+            
+            # If no tenant domain
+            if (!$Tenant.name){
+                $ErrorMessage = "No tenant domain returned."
+                Write-Error $ErrorMessage
+                throw $ErrorMessage
+            }
+
+            # If no email address(es) are specified, request email address
+            while (!$Emails) {
+                $Emails = Read-Host "Enter email address(es), comma separated, to change to $UserType"
+            }
+
+            # Clean emails and create array
+            $Emails = $Emails.Split(",")
+            $Emails = $Emails | ForEach-Object {$_.Trim()}
+
+            foreach ($Email in $Emails){
+                
+                # Get user from email
+                $Email = $Email.replace("@","_")
+                $Email = $Email+"#EXT#@"+$Tenant.name
+                $User = Get-AzureADUser -Filter "UserPrincipalName eq '$Email'"
+
+                # If user exists
+                if ($User) {
+                    $ErrorMessage = "User $Email already exists in the directory."
+                    Write-Error $ErrorMessage
+                }
+                else {                       
+                    # Create external user invitation
+                    New-AzureADMSInvitation `
+                    -InvitedUserEmailAddress $Email `
+                    -SendInvitationMessage $True `
+                    -InviteRedirectUrl "https://portal.azure.com" `
+                    -InvitedUserType $UserType
+
+                    $SuccessMessage = "User $Email has been invited to the directory as $Usertype."
+                    Write-Output $SuccessMessage
+                }
+            }
+        }
+        Catch {
+            Write-Error -Message $_.exception
+            throw $_.exception
+        }
+    }
+    End {
+        
+    }
+}

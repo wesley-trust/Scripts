b8edd429d532abc7ff5a437c6125f3fe2e6bbd13
Wesley Trust <wesley.trust@rosslynanalytics.com>
2017-12-03 16:13:23 +0000

    New function to generate random password string

--- Functions/Azure/AzureAD/RandomPassword.ps1
+++ Functions/Azure/AzureAD/RandomPassword.ps1
@@ -1,3 +1,63 @@
-$Characters = ([char[]]([char]33..[char]95)) + ([char[]]([char]97..[char]126))
-$Randomise = $Characters | Sort-Object {Get-Random}
-$Password = $Randomise[1..8] -join ""
+<#
+#Script name: New-RandomPassword
+#Creator: Wesley Trust
+#Date: 2017-12-03
+#Revision: 1
+#References: 
+
+.Synopsis
+    Function that creates a random password, with default length of 8 characters.
+.Description
+
+.Example
+    New-RandomPassword -CharacterLength $Length
+.Example
+    
+#>
+
+function New-RandomPassword() {
+    Param(
+        [Parameter(
+            Mandatory=$false,
+            HelpMessage="Specify a PowerShell credential"
+        )]
+        [int]
+        $CharacterLength = 8
+    )
+
+    Begin {
+        try {
+
+        }
+        catch {
+            Write-Error -Message $_.Exception
+            throw $_.exception
+        }
+    }
+    
+    Process {
+        try {
+            # Specify character set
+            $CharacterSet = ([char[]]([char]33..[char]95)) + ([char[]]([char]97..[char]126))
+            
+            # Randomise
+            $RandomisedSet = $CharacterSet | Sort-Object {Get-Random}
+            
+            # Specify length
+            $Password = $RandomisedSet[1..$CharacterLength]
+            
+            # Join objects to form password string
+            $Password = $Password -join ""
+            
+            # Return password
+            return $Password
+        }
+        Catch {
+            Write-Error -Message $_.exception
+            throw $_.exception
+        }
+    }
+    End {
+        
+    }
+}

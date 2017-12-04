1543448bd4ec290bdf21057ddf05fd60544a73f5
Wesley Trust <wesley.trust@rosslynanalytics.com>
2017-12-04 22:04:48 +0000

    Remove commented section

--- Functions/Azure/Compute/New-VM.ps1
+++ Functions/Azure/Compute/New-VM.ps1
@@ -171,8 +171,7 @@ function New-VM() {
             . .\Connect-AzureRM.ps1
             
             # Authenticate with Azure
-            $AzureConnection = Connect-AzureRM -SubscriptionID $SubscriptionID #`
-            #| Tee-Object -Variable AzureConnection
+            $AzureConnection = Connect-AzureRM -SubscriptionID $SubscriptionID
 
             # Update subscription Id from Azure Connection
             $SubscriptionID = $AzureConnection.Subscription.id

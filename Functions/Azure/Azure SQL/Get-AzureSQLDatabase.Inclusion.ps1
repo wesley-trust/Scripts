243758a9263f10e43527ceead7da6ab3ddc3c477
Wesley Trust <wesley.trust@rosslynanalytics.com>
2017-12-04 20:26:43 +0000

    Update to SQL database functions to include pool inclusion/exclusion

---
+++ Functions/Azure/Azure SQL/Get-AzureSQLDatabase.Inclusion.ps1
@@ -0,0 +1,178 @@
+<#
+
+#Script name: Get-AzureSQLDatabase
+#Creator: Wesley Trust
+#Date: 2017-12-03
+#Revision: 3
+#References:
+
+.Synopsis
+    A script that gets SQL databases on a specific server, excluding any specified pools.
+.DESCRIPTION
+    A script that gets SQL databases on a specific server, excluding any specified pools,
+    when no SQL pools are specified, all databases on the server are returned (excluding master),
+    includes error checking for whether the SQL server exists.
+#>
+
+
+function Get-AzureSQLDatabase() {
+    Param(
+        [Parameter(
+            Mandatory=$false,
+            HelpMessage="Enter the subscription ID"
+        )]
+        [string]
+        $SubscriptionID,    
+        [Parameter(
+            Mandatory=$false,
+            HelpMessage="Enter the resource group that all VMs belong to"
+        )]
+        [string]
+        $ResourceGroupName,
+        [Parameter(
+            Mandatory=$false,
+            HelpMessage="Enter the SQL Server to check"
+        )]
+        [string]
+        $SQLServer,
+        [Parameter(
+            Mandatory=$false,
+            HelpMessage="Enter the SQL Elastic Pools to check (if any)"
+        )]
+        [string[]]
+        $SQLPools,
+        [Parameter(
+            Mandatory=$false,
+            HelpMessage="Specify whether to exclude database from specified pools"
+        )]
+        [bool]
+        $SQLPoolInclusion = $true,
+        [Parameter(
+            Mandatory=$false,
+            HelpMessage="Specify whether to email results"
+        )]
+        [bool]
+        $Email = $false,
+        [Parameter(
+            Mandatory=$false,
+            HelpMessage="Enter the email server username"
+        )]
+        [string]
+        $EmailUsername,
+        [Parameter(
+            Mandatory=$false,
+            HelpMessage="Enter the email server password"
+        )]
+        [string]
+        $PlainTextPass,
+        [Parameter(
+            Mandatory=$false,
+            HelpMessage="Enter the SMTP Server"
+        )]
+        [string]
+        $SMTPServer,
+        [Parameter(
+            Mandatory=$false,
+            HelpMessage="Enter the recipient email address"
+        )]
+        [string]
+        $ToAddress,
+        [Parameter(
+            Mandatory=$false,
+            HelpMessage="Enter the sender email address"
+        )]
+        [string]
+        $FromAddress
+    )
+
+    Begin {
+        try {
+            # Load functions
+            Set-Location "$ENV:USERPROFILE\GitHub\Scripts\Functions\Azure\Authentication\"
+            . .\Connect-AzureRM.ps1
+            
+            # Connect to Azure
+            Connect-AzureRM -SubscriptionID $SubscriptionID
+        }
+        catch {
+            Write-Error -Message $_.Exception
+            throw $_.exception
+        }
+    }
+    Process {
+        try {
+            # Load functions
+            Set-Location "$ENV:USERPROFILE\GitHub\Scripts\Functions\Azure\Azure SQL"
+            . Get-AzureSQLServer.ps1
+            
+            # Get SQL Server
+            $SQLServer = Get-AzureSQLServer `
+                -SubscriptionID $SubscriptionID `
+                -ResourceGroupName $ResourceGroupName `
+                -SQLServer $SQLServer
+            
+            if (!$SQLServer){
+                $ErrorMessage = "No SQL Server returned."
+                Write-Error $ErrorMessage
+                throw $ErrorMessage
+            }
+
+            # Get all databases from SQL server
+            $SQLDatabases = Get-AzureRmSqlDatabase `
+                -ResourceGroupName $SQLServer.ResourceGroupName `
+                -ServerName $SQLServer.ServerName
+
+            # If SQL Pool inclusion is true
+            if ($SQLPoolInclusion){
+    
+                # Set databases variable to only contain databases that are within a pool in the SQL pools array
+                $SQLDatabases = $SQLDatabases | Where-Object {$SQLPools -contains $($_.elasticpoolname)}
+            }
+
+            # Exclude master database
+            $SQLDatabases = $SQLDatabases | Where-Object {$_.DatabaseName -notmatch "Master"}
+
+            # If there are databases
+            if ($SQLDatabases){
+                
+                # If email notification is enabled
+                If ($Email) {
+
+                    # Set subject and body
+                    $Subject =  "Databases not in an Elastic Pool on SQL Server "+$SQLServer.Servername
+                    $Body = $SQLDatabases.DatabaseName
+                    $Body = [string]::join("<br/>",$body)
+
+                    
+                    # Build Email Credential
+                    $EmailPassword = ConvertTo-SecureString $PlainTextPass -AsPlainText -Force
+                    $EmailCredential = New-Object System.Management.Automation.PSCredential ($EmailUsername, $EmailPassword)
+                    
+                    # Send email
+                    Send-MailMessage `
+                        -Credential $EmailCredential `
+                        -SmtpServer $SMTPServer `
+                        -To $ToAddress `
+                        -From $FromAddress `
+                        -Subject $Subject `
+                        -BodyAsHtml `
+                        -Body $Body
+                }
+                else {
+                    # Display database names
+                    $SQLDatabases.DatabaseName
+                }
+            }
+            Else {
+                Write-Output "No databases found."
+            }
+        }
+        catch {
+            Write-Error -Message $_.Exception
+            throw $_.Exception
+        }
+    }
+    End {
+
+    }
+}

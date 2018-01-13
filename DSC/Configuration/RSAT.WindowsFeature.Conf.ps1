93a2aec38385e13f54f9c1a608af5d21c3ffb745
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2018-01-13 18:11:57 +0000

    Update to fix incorrect placement

--- DSC/Configuration/RSAT.WindowsFeature.Conf.ps1
+++ DSC/Configuration/RSAT.WindowsFeature.Conf.ps1
@@ -1,9 +1,9 @@
-# Import module
-Import-DscResource –ModuleName 'PSDesiredStateConfiguration'
-
-# Configuration definition
+# DSC configuration definition
 Configuration RSAT {
-
+    
+    # Import module
+    Import-DscResource -ModuleName 'PSDesiredStateConfiguration'
+    
     Node RequiresRSAT {
         WindowsFeature MyFeatureInstance {
             Ensure = "Present"

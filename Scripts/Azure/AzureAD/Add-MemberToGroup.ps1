3270cba0e81e9d81a6068d8d3b16fb2fb4599432
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2018-08-28 12:29:40 +0100

    Remove non-required parameter

--- Scripts/Azure/AzureAD/Add-MemberToGroup.ps1
+++ Scripts/Azure/AzureAD/Add-MemberToGroup.ps1
@@ -1,45 +1,57 @@
 <#
-#Script name: Set-AzureAD-UserType
+#Script name: User SKU status
 #Creator: Wesley Trust
-#Date: 2017-12-03
+#Date: 2018-06-16
 #Revision: 1
 #References: 
-
 .Synopsis
-    Function that connects to an Azure AD tenant and sets directory user type (by default to Member).
+    Script to call function to get the Sku status of users, allows specific users or skus to be specified.
 .Description
-
-.Example
-    Set-AzureAD-UserType -Credential $Credential -Emails "wesley.trust@example.com" -UserType $UserType
-.Example
-    
+    By default only returns users and skus that are assigned, formats output, tests for active connection/connects/disconnects, supports reauthentication.
 #>
+
 Param(
     [Parameter(
         Mandatory = $false,
-        HelpMessage = "Specify a PowerShell credential"
-    )]
-    [pscredential]
-    $Credential,
-    [Parameter(
-        Mandatory = $false,
-        HelpMessage = "Specify email address(es) of directory to change"
+        HelpMessage = "Specify the UPN of a user to check, multiple UPNs can be comma separated or in an array, accepts pipeline values",
+        ValueFromPipeLine = $true,
+        ValueFromPipeLineByPropertyName = $true
     )]
     [string[]]
-    $Emails,
+    $UserPrincipalName,
     [Parameter(
         Mandatory = $false,
-        HelpMessage = "Specify user type to change to (Default: Member)"
+        HelpMessage = "Specify the SKU id to check, multiple SKUs can be comma separated or in an array",
+        ValueFromPipeLineByPropertyName = $true
     )]
-    [ValidateSet("Guest", "Member")] 
-    [string]
-    $UserType = "Member",
+    [string[]]
+    $SkuId,
+    [Parameter(
+        Mandatory = $false,
+        HelpMessage = "Specify whether to include users with unassigned skus",
+        ValueFromPipeLineByPropertyName = $true
+    )]
+    [switch]
+    $IncludeUnassignedUser,
+    [Parameter(
+        Mandatory = $false,
+        HelpMessage = "Specify whether to include unassigned subscribed skus",
+        ValueFromPipeLineByPropertyName = $true
+    )]
+    [switch]
+    $IncludeUnassignedSku,
     [Parameter(
         Mandatory = $false,
         HelpMessage = "Specify whether to skip dependency checks"
     )]
     [switch]
     $SkipDependencyCheck,
+    [Parameter(
+        Mandatory = $false,
+        HelpMessage = "Specify a PowerShell credential"
+    )]
+    [pscredential]
+    $Credential,
     [Parameter(
         Mandatory = $false,
         HelpMessage = "Specify whether to skip disconnection"
@@ -56,12 +68,13 @@ Param(
 
 Begin {
     try {
+            
         # Dot source function definitions
         $FunctionLocation = "$ENV:USERPROFILE\GitHub\Scripts\Functions"
         $Functions = @(
             "$FunctionLocation\Toolkit\Invoke-DependencyCheck.ps1",
             "$FunctionLocation\Azure\AzureAD\Test-AzureADConnection.ps1"
-            "$FunctionLocation\Azure\AzureAD\Set-AzureADUserType.ps1"
+            "$FunctionLocation\Azure\AzureAD\UserSkuStatus.ps1"
         )
         foreach ($Function in $Functions) {
             . $Function
@@ -69,13 +82,13 @@ Begin {
 
         # Skip dependency check if switch is true
         if (!$SkipDependencyCheck) {
-    
+            
             # Dependency check for required module:
             $Module = "AzureAD"
 
             Invoke-DependencyCheck -Modules $Module
         }
-
+        
         # Check for active connection to Azure AD
         if (!$ReAuthenticate) {
             $TestConnection = Test-AzureADConnection -Credential $Credential
@@ -98,11 +111,11 @@ Begin {
         }
     }
     catch {
-        Write-Error -Message $_.Exception
+        Write-Error -Message $_.exception
         throw $_.exception
     }
 }
-    
+
 Process {
     try {
 
@@ -116,15 +129,24 @@ Process {
         }
 
         # Call function
-        Set-AzureADUserType `
-            -Emails $Emails `
-            -UserType $UserType
+        $AzureADUserSkuStatus = Get-AzureADUserSkuStatus
+
+        # Check, output and format
+        if ($AzureADUserSkuStatus) {
+            $AzureADUserSkuStatus | Format-Table -AutoSize
+        }
+        else {
+            $ErrorMessage = "No user sku licence status returned"
+            Write-Error $ErrorMessage
+            throw $ErrorMessage
+        }
     }
-    Catch {
+    catch {
         Write-Error -Message $_.exception
         throw $_.exception
     }
 }
+
 End {
     try {
         

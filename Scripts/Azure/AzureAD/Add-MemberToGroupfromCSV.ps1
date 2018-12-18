dd6ae519821e8a3f3264ddc3b8681b262eb44b1e
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2018-12-18 14:03:52 +0000

    Script to add objects to group from a CSV import

--- Scripts/Azure/AzureAD/Add-MemberToGroupfromCSV.ps1
+++ Scripts/Azure/AzureAD/Add-MemberToGroupfromCSV.ps1
@@ -1,45 +1,30 @@
 <#
-#Script name: User SKU status
+#Script name: Add member to group from CSV import of object Ids
 #Creator: Wesley Trust
-#Date: 2018-06-16
+#Date: 2018-12-18
 #Revision: 1
 #References: 
 .Synopsis
-    Script to call function to get the Sku status of users, allows specific users or skus to be specified.
+    Script to add members to a group
 .Description
-    By default only returns users and skus that are assigned, formats output, tests for active connection/connects/disconnects, supports reauthentication.
+
 #>
 
 Param(
     [Parameter(
         Mandatory = $false,
-        HelpMessage = "Specify the UPN of a user to check, multiple UPNs can be comma separated or in an array, accepts pipeline values",
-        ValueFromPipeLine = $true,
+        HelpMessage = "Specify the path of the CSV import file",
         ValueFromPipeLineByPropertyName = $true
     )]
-    [string[]]
-    $UserPrincipalName,
+    [string]
+    $PathToCSV,
     [Parameter(
         Mandatory = $false,
-        HelpMessage = "Specify the SKU id to check, multiple SKUs can be comma separated or in an array",
+        HelpMessage = "Specify the object ID of the group, accepts pipeline values",
         ValueFromPipeLineByPropertyName = $true
     )]
-    [string[]]
-    $SkuId,
-    [Parameter(
-        Mandatory = $false,
-        HelpMessage = "Specify whether to include users with unassigned skus",
-        ValueFromPipeLineByPropertyName = $true
-    )]
-    [switch]
-    $IncludeUnassignedUser,
-    [Parameter(
-        Mandatory = $false,
-        HelpMessage = "Specify whether to include unassigned subscribed skus",
-        ValueFromPipeLineByPropertyName = $true
-    )]
-    [switch]
-    $IncludeUnassignedSku,
+    [string]
+    $GroupObjectId
     [Parameter(
         Mandatory = $false,
         HelpMessage = "Specify whether to skip dependency checks"
@@ -128,17 +113,12 @@ Process {
             }
         }
 
-        # Call function
-        $AzureADUserSkuStatus = Get-AzureADUserSkuStatus
+        # Import CSV
+        $CSVImport = Import-Csv $PathToCSV
 
-        # Check, output and format
-        if ($AzureADUserSkuStatus) {
-            $AzureADUserSkuStatus | Format-Table -AutoSize
-        }
-        else {
-            $ErrorMessage = "No user sku licence status returned"
-            Write-Error $ErrorMessage
-            throw $ErrorMessage
+        # Add members to group
+        foreach ($Member in $CSVImport){
+            Add-AzureADGroupMember -ObjectId $GroupObjectId -RefObjectId $Member.ObjectId
         }
     }
     catch {

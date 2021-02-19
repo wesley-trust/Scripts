ea74cf6819e60cfa98a4bdd7217ca36675915d34
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-02-19 16:57:51 +0000

    Import of CA policies

---
+++ Functions/Azure/AzureAD/ConditionalAccess/Import-CAPolicy.ps1
@@ -0,0 +1,203 @@
+<#
+.Synopsis
+    Import all Conditional Access policies to JSON
+.Description
+    This function imports the Conditional Access policies from JSON using the Microsoft Graph API.
+    The following Microsoft Graph API permissions are required for the service principal used for authentication:
+        Policy.ReadWrite.ConditionalAccess
+        Policy.Read.All
+        Directory.Read.All
+        Agreement.Read.All
+        Application.Read.All
+.PARAMETER ClientID
+    Client ID for the Azure AD service principal with Conditional Access Graph permissions
+.PARAMETER ClientSecret
+    Client secret for the Azure AD service principal with Conditional Access Graph permissions
+.PARAMETER TenantName
+    The initial domain (onmicrosoft.com) of the tenant
+.PARAMETER AccessToken
+    The access token, obtained from executing Get-MSGraphAccessToken
+.PARAMETER FilePath
+    The file path to the JSON file that will be imported
+.PARAMETER PolicyState
+    Modify the policy state when imported, when not specified the policy will maintain state
+.PARAMETER FilePath
+    Specify whether existing policies will be removed during the import operation
+.PARAMETER ExcludePreviewFeatures
+    Specify whether to exclude features in preview, a production API version will then be used instead
+.INPUTS
+    JSON file with all Conditional Access policies
+.OUTPUTS
+    None
+.NOTES
+    Reference: https://danielchronlund.com/2018/11/19/fetch-data-from-microsoft-graph-with-powershell-paging-support/
+.Example
+    $Parameters = @{
+                ClientID = ""
+                ClientSecret = ""
+                TenantDomain = ""
+                FilePath = ""
+    }
+    Import-CAPolicy @Parameters
+    $AccessToken | Import-CAPolicy
+#>
+
+function Import-CAPolicy {
+    [cmdletbinding()]
+    param (
+        [parameter(
+            Mandatory = $false,
+            ValueFromPipeLineByPropertyName = $true,
+            HelpMessage = "Client ID for the Azure AD service principal with Conditional Access Graph permissions"
+        )]
+        [string]$ClientID,
+        [parameter(
+            Mandatory = $false,
+            ValueFromPipeLineByPropertyName = $true,
+            HelpMessage = "Client secret for the Azure AD service principal with Conditional Access Graph permissions"
+        )]
+        [string]$ClientSecret,
+        [parameter(
+            Mandatory = $false,
+            ValueFromPipeLineByPropertyName = $true,
+            HelpMessage = "The initial domain (onmicrosoft.com) of the tenant"
+        )]
+        [string]$TenantDomain,
+        [parameter(
+            Mandatory = $false,
+            ValueFromPipeLineByPropertyName = $true,
+            HelpMessage = "The access token, obtained from executing Get-MSGraphAccessToken"
+        )]
+        [string]$AccessToken,
+        [parameter(
+            Mandatory = $false,
+            ValueFromPipeLineByPropertyName = $true,
+            HelpMessage = "The file path to the JSON file that will be imported"
+        )]
+        [string]$FilePath,
+        [Parameter(
+            Mandatory = $false,
+            ValueFromPipeLineByPropertyName = $true,
+            HelpMessage = "If a policy is enabled, modify the policy state when imported if specified"
+        )]
+        [ValidateSet("enabledForReportingButNotEnforced", "disabled", "")]
+        [AllowNull()]
+        [String]
+        $PolicyState,
+        [Parameter(
+            Mandatory = $false,
+            ValueFromPipeLineByPropertyName = $true,
+            HelpMessage = "Specify whether existing policies will be removed during the import operation"
+        )]
+        [switch]
+        $RemoveAllExistingPolicies,
+        [parameter(
+            Mandatory = $false,
+            ValueFromPipeLineByPropertyName = $true,
+            HelpMessage = "Specify whether to exclude features in preview, a production API version will then be used instead"
+        )]
+        [switch]$ExcludePreviewFeatures
+    )
+    Begin {
+        try {
+            # Function definitions
+            $FunctionLocation = "$ENV:USERPROFILE\GitHub\Scripts\Functions"
+            $Functions = @(
+                "$FunctionLocation\GraphAPI\Get-MSGraphAccessToken.ps1",
+                "$FunctionLocation\GraphAPI\Invoke-MSGraphQuery.ps1"
+            )
+
+            # Function dot source
+            foreach ($Function in $Functions) {
+                . $Function
+            }
+
+            # Variables
+            $Method = "Post"
+            $ApiVersion = "beta"
+            $Uri = "identity/conditionalAccess/policies"
+
+            # Force TLS 1.2
+            [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+        }
+        catch {
+            Write-Error -Message $_.Exception
+            throw $_.exception
+        }
+    }
+    Process {
+        try {
+            # If there is no access token, obtain one
+            if (!$AccessToken) {
+                [pscustomobject]$AccessToken = Get-MSGraphAccessToken `
+                    -ClientID $ClientID `
+                    -ClientSecret $ClientSecret `
+                    -TenantDomain $TenantDomain
+            }
+            if ($AccessToken) {
+                
+                # Change the API version if features in preview are to be excluded
+                if ($ExcludePreviewFeatures) {
+                    $ApiVersion = "v1.0"
+                }
+
+                # Import policies from JSON file
+                $ConditionalAccessPolicies = Get-Content -Raw -Path $FilePath
+                if ($ConditionalAccessPolicies) {
+                    
+                    # Modify enabled policies to report-only or disabled, if specified
+                    if ($PolicyState -eq "enabledForReportingButNotEnforced") {
+                        $ConditionalAccessPolicies = $ConditionalAccessPolicies -replace '"enabled"', '"enabledForReportingButNotEnforced"'
+                    }
+                    elseif ($PolicyState -eq "disabled") {
+                        $ConditionalAccessPolicies = $ConditionalAccessPolicies -replace '"enabled"', '"disabled"'
+                    }
+
+                    # Remove all existing policies if specified, and policies exist
+                    if ($RemoveAllExistingPolicies) {
+                        $ExistingPolicies = $AccessToken | Invoke-MSGraphQuery `
+                            -Method "Get" `
+                            -Uri $ApiVersion/$Uri
+                        if ($ExistingPolicies.value) {
+                            foreach ($Policy in $ExistingPolicies) {
+                                Start-Sleep -Seconds 1
+                                $AccessToken | Invoke-MSGraphQuery `
+                                    -Method "DELETE" `
+                                    -Uri "$ApiVersion/$Uri/$($Policy.id)" `
+                                | Out-Null
+                            }
+                        }
+                    }
+
+                    # Create policies
+                    $ConditionalAccessPolicies = $ConditionalAccessPolicies | ConvertFrom-Json
+                    foreach ($Policy in $ConditionalAccessPolicies) {
+                        Start-Sleep -Seconds 1
+                        $AccessToken | Invoke-MSGraphQuery `
+                            -Method $Method `
+                            -Uri $ApiVersion/$Uri `
+                            -Body ($Policy `
+                            | ConvertTo-Json -Depth 10) | Out-Null
+                    }
+                }
+                else {
+                    $ErrorMessage = "No Conditional Access policies specified in file"
+                    Write-Error $ErrorMessage
+                    throw $ErrorMessage
+                }
+            }
+            else {
+                $ErrorMessage = "No access token specified, obtain an access token object from Get-MSGraphAccessToken"
+                Write-Error $ErrorMessage
+                throw $ErrorMessage
+            }
+        }
+        catch {
+            Write-Error -Message $_.Exception
+            throw $_.exception
+        }
+    }
+    End {
+        
+    }
+}
